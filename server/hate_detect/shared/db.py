11f9d3d03eda956a2b8736ab1858d9ca98fcdaf5
sqz269 <yin2690855381@gmail.com>
2021-10-09 18:12:46 -0400

    db.execute to cursor.execute

--- server/hate_detect/shared/db.py
+++ server/hate_detect/shared/db.py
@@ -8,7 +8,7 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
-        db.execute(r"CREATE TABLE IF NOT EXIST data (text TEXT, isHate INTEGER)")
+        db.cursor().execute(r"CREATE TABLE IF NOT EXIST data (text TEXT, isHate INTEGER)")
         db.commit()
     return db
 
