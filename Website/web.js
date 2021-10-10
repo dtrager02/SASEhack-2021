7b7344ad118176f620acea9f01bfb6b61c446f5a
Soulz <darint916@gmail.com>
2021-10-10 02:35:27 -0400

    cool table

--- Website/web.js
+++ Website/web.js
@@ -1,1 +1,9 @@
-$("")
+$("table tr").each(function(){
+    let num = parseFloat($(this).find("td").eq(1).text());
+    if(num > .6 || num <.4){
+    $(this).find("td").eq(1).css("background-color",`rgb(${parseInt((1-num)*255)},${parseInt(num*255)},0)`);
+    console.log(num);
+} else {
+    $(this).find("td").eq(1).css("background-color",`rgb(150,150,150)`);
+}
+});
