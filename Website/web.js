a8810b57f06a649f6c337cc51f9a7b70adebef6f
Soulz <darint916@gmail.com>
2021-10-10 03:39:42 -0400

    comment fixes

--- Website/web.js
+++ Website/web.js
@@ -1,9 +1,16 @@
 $("table tr").each(function(){
     let num = parseFloat($(this).find("td").eq(1).text());
     if(num > .6 || num <.4){
-    $(this).find("td").eq(1).css("background-color",`rgb(${parseInt((1-num)*255)},${parseInt(num*255)},0)`);
+    $(this).find("td").eq(1).css("background-color",`rgb(${parseInt((num)*255)},${parseInt((1-num)*255)},0)`);
     console.log(num);
 } else {
     $(this).find("td").eq(1).css("background-color",`rgb(150,150,150)`);
 }
 });
+function clickfun()
+{
+   var input = document.getElementById("val1").value;
+   document.getElementById("val1").value = "Response Recorded!";
+   console.log(input);
+   return input;
+}
