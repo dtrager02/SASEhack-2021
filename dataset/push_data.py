18917cf1b73595611d9378d069cc54333a9c0f44
dtrager02 <dtrager02@gmail.com>
2021-10-10 04:18:03 -0500

    d

--- dataset/push_data.py
+++ dataset/push_data.py
@@ -9,11 +9,11 @@ cursor = db.cursor()
 cursor.execute("DELETE FROM data")
 db.commit()
 
-with open(sheet, "r") as f:
-    f.readline()
-    for line in f:
-        ln = line.split(",")
-        isHate = bool(int(ln[-1]))
-        text = ",".join(ln[:-1])
-        cursor.execute("INSERT INTO data (text, isHate, date) VALUES(?, ?, ?)", (text, isHate, datetime.datetime.now()))
-    db.commit()
+# with open(sheet, "r") as f:
+#     f.readline()
+#     for line in f:
+#         ln = line.split(",")
+#         isHate = bool(int(ln[-1]))
+#         text = ",".join(ln[:-1])
+#         cursor.execute("INSERT INTO data (text, isHate, date) VALUES(?, ?, ?)", (text, isHate, datetime.datetime.now()))
+#     db.commit()
